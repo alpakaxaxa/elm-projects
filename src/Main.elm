89f2e669ec307e047e626ddc320dedd36abe6920
alpakaxaxa <34175210+alpakaxaxa@users.noreply.github.com>
2022-03-27 22:42:46 +0200

    ye

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,75 @@
 module Main exposing (..)
 
+import Array
 import Browser
-import Html exposing (Html, button, div, h1, img, input, li, ol, span, text)
-import Html.Attributes exposing (placeholder, src, style, type_, value)
-import Html.Events exposing (onClick, onInput, onSubmit)
-import Http
-import Json.Decode as JD
-import Json.Encode as JE
+import Debug exposing (log, toString)
+import Html exposing (Html, button, div, h1, h2, img, text)
+import Html.Attributes exposing (class, classList, src)
+import Html.Events exposing (onClick)
+import Random exposing (Seed, generate)
+import Random.List exposing (..)
+import Time
 
 
 
 ---- MODEL ----
 
 
-type alias Model =
-    { todos : List Todo
-    , inputText : String
-    , todo : Todo
-    }
-
-
-type alias Todos =
-    List Todo
-
-
-type alias Todo =
+type alias Question =
     { id : Int
-    , title : String
+    , text : String
+    , correctAnswerLeft : Bool
     , completed : Bool
     }
 
 
+type alias Questions =
+    List Question
+
+
+type alias Error =
+    { message : String
+    , code : Int
+    }
+
+
+type alias Model =
+    { questions : Questions
+    , currentError : Error
+    }
+
+
+type QuestionAnswer
+    = Left
+    | Right
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { todos = [], inputText = "", todo = Todo 0 "" False }, fetchTodos )
+    ( { questions =
+            [ { id = 1
+              , text = "The capital of France is Paris"
+              , correctAnswerLeft = False
+              , completed = False
+              }
+            , { id = 2
+              , text = "The capital of Germany is Munich"
+              , correctAnswerLeft = True
+              , completed = False
+              }
+            , { id = 3
+              , text = "The capital of Italy is Milano"
+              , correctAnswerLeft = True
+              , completed = False
+              }
+            ]
+      , currentError =
+            { message = ""
+            , code = 0
+            }
+      }
+    , Cmd.none
+    )
 
 
 
@@ -42,82 +78,87 @@ init =
 
 type Msg
     = NoOp
-    | GetTodos
-    | GotTodos (Result Http.Error Todos)
-    | ChangeInput String
-    | AddTodo
-    | GotTodo (Result Http.Error Todo)
+    | NextQuestion
+    | AnswerQuestion Int QuestionAnswer
+    | ShuffleQuestions
+    | ShuffledQuestions (List Question)
+    | Restart
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
+    {- let
+           logmessage1 =
+               log (toString model.questions)
+       in
+    -}
     case msg of
         NoOp ->
             ( model, Cmd.none )
 
-        GetTodos ->
-            ( model, fetchTodos )
+        NextQuestion ->
+            ( model, Cmd.none )
 
-        GotTodos result ->
-            case result of
-                Ok todos ->
-                    ( { model | todos = todos }, Cmd.none )
+        AnswerQuestion questionIndex questionAnswer ->
+            if isAnswerCorrect model.questions questionIndex questionAnswer then
+                ( { model | questions = answerQuestion model.questions questionIndex questionAnswer, currentError = Error "" 0 }, Cmd.none )
 
-                Err _ ->
-                    ( model, Cmd.none )
+            else
+                ( { model | currentError = Error "Wrong answer" 1 }
+                , Cmd.none
+                )
 
-        ChangeInput text ->
-            ( { model | inputText = text }, Cmd.none )
+        ShuffleQuestions ->
+            ( model, generate ShuffledQuestions (shuffle model.questions) )
 
-        AddTodo ->
-            ( model, postTodo model )
+        ShuffledQuestions questions ->
+            ( { model | questions = questions }, Cmd.none )
 
-        GotTodo result ->
-            case result of
-                Ok todo ->
-                    ( { model | todo = todo, inputText = "" }, Cmd.none )
-
-                Err _ ->
-                    ( model, Cmd.none )
+        Restart ->
+            ( { model | questions = resetQuestions model.questions }, Cmd.none )
 
 
-fetchTodos : Cmd Msg
-fetchTodos =
-    Http.get
-        { url = "https://jsonplaceholder.typicode.com/todos"
-        , expect = Http.expectJson GotTodos todosDecoder
-        }
+answerQuestion questions questionIndex questionAnswer =
+    List.map
+        (\question ->
+            if questionIndex == question.id && checkQuestionAnswer questionAnswer question.correctAnswerLeft then
+                { question | completed = True }
+
+            else
+                question
+        )
+        questions
 
 
-todoParser =
-    JD.map3 Todo
-        (JD.field "id" JD.int)
-        (JD.field "title" JD.string)
-        (JD.field "completed" JD.bool)
+isAnswerCorrect questions questionIndex questionAnswer =
+    Array.fromList questions
+        |> (\array ->
+                case Array.get (questionIndex - 1) array of
+                    Nothing ->
+                        False
+
+                    Just question ->
+                        checkQuestionAnswer questionAnswer question.correctAnswerLeft
+           )
 
 
-todosDecoder =
-    JD.list todoParser
+checkQuestionAnswer questionAnswer hasCorrectAnswerLeft =
+    if questionAnswer == Left && hasCorrectAnswerLeft then
+        True
+
+    else if questionAnswer == Right && not hasCorrectAnswerLeft then
+        True
+
+    else
+        False
 
 
-todoEncode : String -> JE.Value
-todoEncode inputText =
-    JE.object
-        [ ( "id", JE.int 0 )
-        , ( "title", JE.string inputText )
-        , ( "completed", JE.bool False )
-        ]
-
-
-postTodo : Model -> Cmd Msg
-postTodo model =
-    Http.post
-        { url = "https://jsonplaceholder.typicode.com/todos"
-        , body =
-            todoEncode model.inputText
-                |> Http.jsonBody
-        , expect = Http.expectJson GotTodo todoParser
-        }
+resetQuestions questions =
+    List.map
+        (\question ->
+            { question | completed = False }
+        )
+        questions
 
 
 
@@ -126,25 +167,76 @@ postTodo model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Your todos" ]
-        , div [] [ input [ type_ "text", value model.inputText, onInput ChangeInput ] [] ]
-        , div [] [ button [ onClick AddTodo ] [ text "Submit todo" ] ]
-        , ol [] (List.take 10 (List.map todoHtmlView model.todos))
-        ]
-
-
-todoHtmlView : Todo -> Html Msg
-todoHtmlView todo =
-    li []
-        [ span []
-            [ text todo.title
+    div [ class "section" ]
+        [ div [ class "container" ]
+            [ h1 [ class "title" ] [ text "Your Elm App is working!", h2 [ class "subtitle" ] [ text "Press e for false end i for true" ] ]
+            , div [] [ viewQuestionHtml ( getCurrentQuestion model.questions, model.currentError ) ]
             ]
         ]
 
 
+viewQuestionHtml : ( Question, Error ) -> Html Msg
+viewQuestionHtml ( question, error ) =
+    Html.section [ class "hero is-fullheight" ]
+        [ div [ class "hero-body" ]
+            [ viewQuestionCardHtml ( question, error )
+            ]
+        ]
+
+
+viewQuestionCardHtml : ( Question, Error ) -> Html Msg
+viewQuestionCardHtml ( question, error ) =
+    if question.id /= 0 then
+        div [ class "card" ]
+            [ displayError error
+            , div [ class "card-content" ] [ text question.text ]
+            , Html.footer [ class "card-footer" ]
+                [ div [ class "card-footer-item" ]
+                    [ button [ class "button", onClick (AnswerQuestion question.id Left) ] [ text "False" ]
+                    ]
+                , div [ class "card-footer-item" ]
+                    [ button [ class "button", onClick (AnswerQuestion question.id Right) ] [ text "True" ]
+                    ]
+                ]
+            ]
+
+    else
+        div []
+            [ div [] [ text "Contratulations you answered everything correctly!" ]
+            , div [ class "button", onClick Restart ] [ text "Play again!" ]
+            ]
+
+
+displayError error =
+    if error.code == 0 then
+        div [] []
+
+    else
+        div [ class "notification is-danger" ] [ text error.message ]
+
+
+getCurrentQuestion : Questions -> Question
+getCurrentQuestion questions =
+    List.filter (\question -> not question.completed) questions
+        |> hasValidQuestion
+
+
+hasValidQuestion : Questions -> Question
+hasValidQuestion questions =
+    case List.head questions of
+        Nothing ->
+            Question 0 "" False False
+
+        Just question ->
+            question
+
+
 
 ---- PROGRAM ----
+{- subscriptions : Model -> Sub Msg
+   subscriptions model =
+       onClick ShuffleQuestions
+-}
 
 
 main : Program () Model Msg
